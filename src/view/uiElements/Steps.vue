03098e67e5e13d82d810ba8c3d08162e540bec56
tonmoy773 <mdkhairul773@gmail.com>
2021-04-01 09:07:27 +0600

    Steps Element Update.

--- src/view/uiElements/Steps.vue
+++ src/view/uiElements/Steps.vue
@@ -16,11 +16,27 @@
     <Main>
       <a-row :gutter="25">
         <a-col :xs="24">
-          <sdCards title="Basic">
+          <sdCards title="Basic Step">
             <basic size="small" :title="['Finished', 'In Progress', 'Waiting']" :current="1" />
           </sdCards>
-          <sdCards title="With Icon">
+          <sdCards title="With Icon Step">
             <basic step="withIcon" size="small" :data="data" />
+          </sdCards>
+          <sdCards title="Switch Step">
+            <basic step="switch" size="small" :data="steps" />
+          </sdCards>
+          <sdCards title="Vertical Step">
+            <basic step="direction" direction="vertical" size="small" :data="dataDescription" :current="2" />
+          </sdCards>
+          <sdCards title="Error Status Step">
+            <basic
+              status="error"
+              step="direction"
+              direction="horizontal"
+              size="small"
+              :data="dataDescription"
+              :current="1"
+            />
           </sdCards>
         </a-col>
       </a-row>
@@ -31,7 +47,7 @@
 <script>
 import { Main } from '../styled';
 import Basic from '../../components/steps/Basic';
-import { data } from '../../demoData/step-data.json';
+import { data, steps, dataDescription } from '../../demoData/step-data.json';
 export default {
   name: 'Steps',
   components: {
@@ -39,7 +55,11 @@ export default {
     Basic,
   },
   setup() {
-    return { data };
+    return {
+      data,
+      dataDescription,
+      steps,
+    };
   },
 };
 </script>
