96e4e14b48d4a3b11b92de0c330696eb7c32cd3d
khairul773 <mdkhairul105islam@gmail.com>
2021-05-03 10:42:32 +0600

    Card Menu item updated.

--- src/components/cards/BannerCard.vue
+++ src/components/cards/BannerCard.vue
@@ -10,17 +10,9 @@
           <div class="more">
             <sdDropdown :action="['click']" class="wide-dropdwon">
               <template #overlay>
-                <a-menu>
-                  <a-menu-item>
-                    <router-link to="#">Edit</router-link>
-                  </a-menu-item>
-                  <a-menu-item>
-                    <router-link to="#">Delete</router-link>
-                  </a-menu-item>
-                  <a-menu-item>
-                    <router-link to="#">View</router-link>
-                  </a-menu-item>
-                </a-menu>
+                <router-link to="#">Edit</router-link>
+                <router-link to="#">Delete</router-link>
+                <router-link to="#">View</router-link>
               </template>
               <router-link to="#">
                 <sdFeatherIcons type="more-horizontal" size="18" />
