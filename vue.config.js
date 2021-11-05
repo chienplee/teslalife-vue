4ce2e122b546c83700ab8c2321ff18e320c08e31
Peter Lee <peterlee@marsler.com>
2021-11-05 14:27:53 +0800

    Add github page feature

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 // eslint-disable-next-line no-global-assign
 require = require('esm')(module);
 module.exports = require('./customize-vue-config.js');
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/teslalife-vue/'
+      : '/'
+  }
